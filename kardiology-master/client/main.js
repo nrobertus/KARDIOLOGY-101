b3124f953ece912d8a81090171d3640bb4ec46b6
Nathan Robertus <nathan.robertus@aed.pro>
2017-01-06 11:56:57 -0700

    Added a template and moved some functions for google authentication into the controller

--- kardiology-master/client/main.js
+++ kardiology-master/client/main.js
@@ -1,15 +1,35 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
-angular.module('kardiology-home', [
+
+var kardiology = angular.module('kardiology-home', [
   angularMeteor
 ]);
 
+kardiology.controller("kardiologyHomeCtrl", ['$scope', function($scope){
+  $scope.attributes = [
+    {text:'attribute 1'},
+    {text:'attribute 2'},
+    {text:'attribute 3'}
+  ];
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
-}
+  function onSignIn(googleUser){
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    $scope.$digest;
+  }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+    $scope.$digest;
+  }
+
+  window.onSignIn = onSignIn;
+  window.signOut = signOut;
+}]);
